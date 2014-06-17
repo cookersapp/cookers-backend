5fa83063209a5c1afd11ba6650ad1d4ef81128fd
Loïc Knuchel <loicknuchel@gmail.com>
2014-06-17 22:40:03 +0200

    refactor app with needed layout

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,37 @@
-'use strict';
+angular.module('firebaseAdminApp', ['ui.router'])
 
-angular
-  .module('firebaseAdminApp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+.config(function($stateProvider, $urlRouterProvider){
+  'use strict';
+  $urlRouterProvider.otherwise('/home');
+
+  $stateProvider
+  .state('app', {
+    url: '',
+    abstract: true,
+    templateUrl: 'views/root.html',
+    controller: 'AppCtrl'
+  })
+  .state('app.home', {
+    url: '/home',
+    templateUrl: 'views/home.html',
+    controller: 'HomeCtrl'
   });
+})
+
+.run(function($rootScope, $location){
+  $rootScope.isActive = function (viewLocation) {
+    var regex = new RegExp('^'+viewLocation+'$', 'g');
+    return regex.test($location.path());
+  };
+
+  $rootScope.safeApply = function(fn){
+    var phase = this.$root ? this.$root.$$phase : this.$$phase;
+    if(phase === '$apply' || phase === '$digest') {
+      if(fn && (typeof(fn) === 'function')) {
+        fn();
+      }
+    } else {
+      this.$apply(fn);
+    }
+  };
+});
---
+++ app/scripts/controllers.js
@@ -0,0 +1,10 @@
+angular.module('firebaseAdminApp')
+
+.controller('AppCtrl', function ($scope) {
+  'use strict';
+  
+})
+.controller('HomeCtrl', function ($scope) {
+  'use strict';
+  
+});
