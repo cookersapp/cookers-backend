3c8e7eb3e9dbe7e5aea1cffa22006baa2319eeef
Loïc Knuchel <loicknuchel@gmail.com>
2014-10-09 14:06:21 +0200

    add dateinfo to events

--- app/models/Event.scala
+++ app/models/Event.scala
@@ -1,19 +1,25 @@
 package models
 
+import play.api.libs.json.Json
 import play.api.libs.json.JsValue
+
+case class DateInfo(year: Int, month: Int, week: Int, dayOfYear: Int, dayOfWeek: Int)
+
+object DateInfo {
+  implicit val dateinfoFormat = Json.format[DateInfo]
+}
 
 case class Event(
   eventId: String,
   previousEventId: Option[String],
   name: String,
   time: Long,
+  dateinfo: DateInfo,
   userId: String,
   source: Option[JsValue],
   data: Option[JsValue],
   debug: Option[Boolean])
 
 object Event {
-  import play.api.libs.json.Json
-
   implicit val eventFormat = Json.format[Event]
 }
